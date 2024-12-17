6f9fe8fc4c4350496b3def0fb0ab7da66b1daf2f
OscarForment <oscarforment@gmail.com>
2024-12-17 18:55:10 +0100

    Change to mipmap and use NumberPicker

--- app/src/main/java/com/example/pokino/MainActivity.kt
+++ app/src/main/java/com/example/pokino/MainActivity.kt
@@ -4,20 +4,14 @@ import android.os.Vibrator
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ToggleButton
 import android.widget.GridLayout
 import android.widget.Toast
 import android.app.AlertDialog
-import android.os.Handler
-import android.os.Looper
 import android.util.TypedValue
-import android.widget.ArrayAdapter
-
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.NumberPicker
 import android.widget.Switch
 import kotlin.random.Random
 
@@ -51,29 +45,30 @@ class MainActivity : Activity() {
         }
         for (i in 0 until 5) {
             for (j in 0 until 5) {
-                buttons[i][j].setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+                buttons[i][j].setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
                 buttons[i][j].textOn = "1"
                 buttons[i][j].textOff = "1"
-                //buttons[i][j].setButtonDrawable(R.drawable.oros)
-                buttons[i][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.oros,0)
+                buttons[i][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0,R.mipmap.oros)
                 buttons[i][j].setOnClickListener { onCardClick(buttons[i][j]) }
 
             }
         }
-        //randomCards()
+
         loadButtonState()
         // Configura el botón de limpieza
         val resetButton: Button = findViewById(R.id.resetButton)
         resetButton.setOnClickListener { resetCards() }
+        // Configura el botón de búsqueda
         val searchButton: Button = findViewById(R.id.searchButton)
         searchButton.setOnClickListener { searchCards() }
+        // Configura el botón de aleatoriedad
         val randomButton: Button = findViewById(R.id.randomButton)
         randomButton.setOnClickListener { confirmationRandomCards() }
 
 
     }
 
-
+    // Guardar el estado de los botones cuando se cierra la aplicación
     private fun saveButtonState() {
         val sharedPref = getPreferences(MODE_PRIVATE)
         val editor = sharedPref.edit()
@@ -83,12 +78,12 @@ class MainActivity : Activity() {
                 val keyText = "buttonText_$i$j"
                 val keyImage = "buttonImage_$i$j"
 
-                val palo = when (buttons[i][j].compoundDrawablesRelative[2]?.constantState) {
-                    resources.getDrawable(R.drawable.oros)?.constantState -> R.drawable.oros
-                    resources.getDrawable(R.drawable.espadas)?.constantState -> R.drawable.espadas
-                    resources.getDrawable(R.drawable.bastos)?.constantState -> R.drawable.bastos
-                    resources.getDrawable(R.drawable.copas)?.constantState -> R.drawable.copas
-                    else -> R.drawable.oros // Manejar caso por defecto o desconocido
+                val palo = when (buttons[i][j].compoundDrawablesRelative[3]?.constantState) {
+                    resources.getDrawable(R.mipmap.oros)?.constantState -> R.mipmap.oros
+                    resources.getDrawable(R.mipmap.espadas)?.constantState -> R.mipmap.espadas
+                    resources.getDrawable(R.mipmap.bastos)?.constantState -> R.mipmap.bastos
+                    resources.getDrawable(R.mipmap.copas)?.constantState -> R.mipmap.copas
+                    else -> R.mipmap.oros // Manejar caso por defecto o desconocido
                 }
 
                 editor.putString(keyText, buttons[i][j].textOn.toString())
@@ -98,11 +93,12 @@ class MainActivity : Activity() {
 
         editor.apply()
     }
-
     override fun onPause() {
         super.onPause()
         saveButtonState()
     }
+
+    // Cargar el estado de los botones cuando se inicia la aplicación
     private fun loadButtonState() {
         val sharedPref = getPreferences(MODE_PRIVATE)
 
@@ -119,7 +115,7 @@ class MainActivity : Activity() {
                 buttons[i][j].text = savedText
 
                 if (savedImage != 0) {
-                    buttons[i][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, savedImage, 0)
+                    buttons[i][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, savedImage)
                 }
             }
         }
@@ -128,30 +124,19 @@ class MainActivity : Activity() {
 
 
     private fun onCardClick(button: ToggleButton) {
-        if( editButton.isChecked){
+        if (editButton.isChecked) {
             val inflater = LayoutInflater.from(this)
             val dialogView = inflater.inflate(R.layout.dialog_edit, null)
 
-            val numberSpinner = dialogView.findViewById<Spinner>(R.id.numberSpinner)
-            val spinner = dialogView.findViewById<Spinner>(R.id.spinner)
+            val numberPicker = dialogView.findViewById<NumberPicker>(R.id.numberPicker)
+            numberPicker.minValue = 1
+            numberPicker.maxValue = 12
 
-            ArrayAdapter.createFromResource(
-                this,
-                R.array.numbers_1_to_12,
-                android.R.layout.simple_spinner_item
-            ).also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                numberSpinner.adapter = adapter
-            }
-
-            ArrayAdapter.createFromResource(
-                this,
-                R.array.suit_options,
-                android.R.layout.simple_spinner_item
-            ).also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.adapter = adapter
-            }
+            val stringNumberPicker = dialogView.findViewById<NumberPicker>(R.id.stringNumberPicker)
+            val suitOptions = resources.getStringArray(R.array.suit_options)
+            stringNumberPicker.minValue = 0
+            stringNumberPicker.maxValue = suitOptions.size - 1
+            stringNumberPicker.displayedValues = suitOptions
 
             val alertDialogBuilder = AlertDialog.Builder(this)
             alertDialogBuilder.setView(dialogView)
@@ -159,102 +144,84 @@ class MainActivity : Activity() {
             alertDialogBuilder
                 .setCancelable(false)
                 .setPositiveButton("Guardar") { _, _ ->
-                    val newText = numberSpinner.selectedItem.toString()
-                    val selectedSuit = spinner.selectedItem.toString()
-                    updateButtonText(newText,selectedSuit,button)
+                    val newText = numberPicker.value.toString()
+                    val selectedSuit = suitOptions[stringNumberPicker.value]
+                    updateButtonText(newText, selectedSuit, button)
                 }
                 .setNegativeButton("Cancelar") { dialog, _ ->
-                    button.isChecked=!button.isChecked
+                    button.isChecked = !button.isChecked
                     dialog.cancel()
                 }
 
             val alertDialog = alertDialogBuilder.create()
             alertDialog.show()
 
-        }else{
-            if(!centro.isChecked){
-                if (buttons[2][2].isChecked){
-                    centro.isChecked=true
+        } else {
+            if (!centro.isChecked) {
+                if (buttons[2][2].isChecked) {
+                    centro.isChecked = true
                     Toast.makeText(this, "¡CENTRO!", Toast.LENGTH_SHORT).show()
                     vibratePhone()
                 }
-
             }
-            if(!esquinas.isChecked){
+            if (!esquinas.isChecked) {
                 if (buttons[0][0].isChecked && buttons[4][4].isChecked
-                    && buttons[4][0].isChecked && buttons[0][4].isChecked){
-                    esquinas.isChecked=true
+                    && buttons[4][0].isChecked && buttons[0][4].isChecked) {
+                    esquinas.isChecked = true
                     Toast.makeText(this, "¡ESQUINAS!", Toast.LENGTH_SHORT).show()
                     vibratePhone()
                 }
-
             }
-            if(!pokino.isChecked){
+            if (!pokino.isChecked) {
                 if (buttons[0][0].isChecked && buttons[0][1].isChecked
-                    && buttons[0][2].isChecked && buttons[0][3].isChecked){
-                    pokino.isChecked=true
+                    && buttons[0][2].isChecked && buttons[0][3].isChecked) {
+                    pokino.isChecked = true
                     Toast.makeText(this, "¡POKINO!", Toast.LENGTH_SHORT).show()
                     vibratePhone()
                 }
-
             }
-            if(!linea.isChecked){
+            if (!linea.isChecked) {
                 if (checkHorizontalLine() || checkVerticalLine()) {
                     linea.isChecked = true
                     Toast.makeText(this, "¡LÍNEA!", Toast.LENGTH_SHORT).show()
                     vibratePhone()
                 }
-
             }
-
         }
-
     }
-
+   // Función para buscar cartas
     private fun searchCards() {
-            val inflater = LayoutInflater.from(this)
-            val dialogView = inflater.inflate(R.layout.dialog_edit, null)
+        val inflater = LayoutInflater.from(this)
+        val dialogView = inflater.inflate(R.layout.dialog_edit, null)
 
-            val numberSpinner = dialogView.findViewById<Spinner>(R.id.numberSpinner)
-            val spinner = dialogView.findViewById<Spinner>(R.id.spinner)
+        val numberPicker = dialogView.findViewById<NumberPicker>(R.id.numberPicker)
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 12
 
-            ArrayAdapter.createFromResource(
-                this,
-                R.array.numbers_1_to_12,
-                android.R.layout.simple_spinner_item
-            ).also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                numberSpinner.adapter = adapter
+        val stringNumberPicker = dialogView.findViewById<NumberPicker>(R.id.stringNumberPicker)
+        val suitOptions = resources.getStringArray(R.array.suit_options)
+        stringNumberPicker.minValue = 0
+        stringNumberPicker.maxValue = suitOptions.size - 1
+        stringNumberPicker.displayedValues = suitOptions
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setView(dialogView)
+
+        alertDialogBuilder
+            .setCancelable(false)
+            .setPositiveButton("Buscar") { _, _ ->
+                val selectedNumber = numberPicker.value.toString()
+                val selectedSuit = suitOptions[stringNumberPicker.value]
+                searchCard(selectedNumber, selectedSuit)
+            }
+            .setNegativeButton("Cancelar") { dialog, _ ->
+                dialog.dismiss()
             }
 
-            ArrayAdapter.createFromResource(
-                this,
-                R.array.suit_options,
-                android.R.layout.simple_spinner_item
-            ).also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.adapter = adapter
-            }
-
-            val alertDialogBuilder = AlertDialog.Builder(this)
-            alertDialogBuilder.setView(dialogView)
-
-            alertDialogBuilder
-                .setCancelable(false)
-                .setPositiveButton("Buscar") { _, _ ->
-                    val newText = numberSpinner.selectedItem.toString()
-                    val selectedSuit = spinner.selectedItem.toString()
-                    searchCard(newText,selectedSuit)
-                }
-                .setNegativeButton("Cancelar") { dialog, _ ->
-                    dialog.cancel()
-                }
-
-            val alertDialog = alertDialogBuilder.create()
-            alertDialog.show()
-
-        }
-
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+    // Función para reiniciar los botones
     private fun resetCards() {
 
         Toast.makeText(this, "Reiniciando", Toast.LENGTH_SHORT).show()
@@ -271,11 +238,11 @@ class MainActivity : Activity() {
         pokino.isChecked=false
         linea.isChecked=false
     }
-
+    // Función para confirmar la aleatoriedad de las cartas
     private fun confirmationRandomCards() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Confirmar acción")
-        builder.setMessage("¿Estás seguro de que deseas randomizar las cartas?")
+        builder.setMessage("¿Estás seguro de que deseas cambiar tu cartón?")
 
         builder.setPositiveButton("Sí") { _, _ ->
             // Si el usuario hace clic en Sí, llama a la función randomCards()
@@ -289,15 +256,16 @@ class MainActivity : Activity() {
         val dialog = builder.create()
         dialog.show()
     }
+    // Función para generar cartas aleatorias
     private fun randomCards() {
         val (numpalo, numero) = generateNumbers()
-        val palos = intArrayOf(R.drawable.oros, R.drawable.espadas, R.drawable.bastos, R.drawable.copas)
+        val palos = intArrayOf(R.mipmap.oros, R.mipmap.espadas, R.mipmap.bastos, R.mipmap.copas)
         palos.shuffle()
         for (j in 0 until 4) {
 
             buttons[0][j].textOn = numero
             buttons[0][j].textOff = numero
-            buttons[0][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,palos[j],0)
+            buttons[0][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0,palos[j])
             // Forzar la actualización del texto
             buttons[0][j].text = numero
 
@@ -312,15 +280,15 @@ class MainActivity : Activity() {
         }while(numeroesq==numero)
 
         val palo = when (numpaloesq) {
-            1 -> R.drawable.oros
-            2 -> R.drawable.espadas
-            3 -> R.drawable.bastos
-            4 -> R.drawable.copas
-            else -> R.drawable.oros // Manejar caso por defecto o desconocido
+            1 -> R.mipmap.oros
+            2 -> R.mipmap.espadas
+            3 -> R.mipmap.bastos
+            4 -> R.mipmap.copas
+            else -> R.mipmap.oros // Manejar caso por defecto o desconocido
         }
         buttons[0][4].textOn = numeroesq
         buttons[0][4].textOff = numeroesq
-        buttons[0][4].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,palo,0)
+        buttons[0][4].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0,palo)
         // Forzar la actualización del texto
         buttons[0][4].text = numeroesq
         for (i in 1 until 5) {
@@ -333,19 +301,20 @@ class MainActivity : Activity() {
         }
 
     }
+    // Función auxiliar para generar cartas aleatorias y que no se repitan
     private fun generateCard(i:Int,j:Int){
         val (numpalo, numero) = generateNumbers()
         val palo = when (numpalo) {
-            1 -> R.drawable.oros
-            2 -> R.drawable.espadas
-            3 -> R.drawable.bastos
-            4 -> R.drawable.copas
-            else -> R.drawable.oros // Manejar caso por defecto o desconocido
+            1 -> R.mipmap.oros
+            2 -> R.mipmap.espadas
+            3 -> R.mipmap.bastos
+            4 -> R.mipmap.copas
+            else -> R.mipmap.oros // Manejar caso por defecto o desconocido
         }
         var repe = false
         for (ii in 0 until i) {
             for (jj in 0 until 5) {
-                val currentDrawable = buttons[ii][jj].compoundDrawablesRelative[2]
+                val currentDrawable = buttons[ii][jj].compoundDrawablesRelative[3]
                 if (buttons[ii][jj].textOff == numero &&
                     currentDrawable?.constantState == resources.getDrawable(palo)?.constantState
                 ) {
@@ -354,7 +323,7 @@ class MainActivity : Activity() {
             }
         }
         for (jj in 0 until j) {
-            val currentDrawable = buttons[i][jj].compoundDrawablesRelative[2]
+            val currentDrawable = buttons[i][jj].compoundDrawablesRelative[3]
             if (buttons[i][jj].textOff == numero &&
                 currentDrawable?.constantState == resources.getDrawable(palo)?.constantState
             ) {
@@ -366,11 +335,12 @@ class MainActivity : Activity() {
         }else{
             buttons[i][j].textOn = numero
             buttons[i][j].textOff = numero
-            buttons[i][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,palo,0)
+            buttons[i][j].setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0,palo)
             // Forzar la actualización del texto
             buttons[i][j].text = numero
         }
     }
+    // Función para generar el palo y el numero de las cartas
     private fun generateNumbers(): Pair<Int,String>{
         val numeroEntre1y4 = Random.nextInt(1, 5)
         var numeroEntre1y12: Int
@@ -379,20 +349,21 @@ class MainActivity : Activity() {
         } while (numeroEntre1y12 == 8 || numeroEntre1y12 == 9)
         return Pair(numeroEntre1y4, numeroEntre1y12.toString())
     }
+    // Función para buscar cartas
     private fun searchCard(newText: String, selectedSuit: String) {
         val buttonTextWithSuit = when (selectedSuit) {
-            "oros" -> R.drawable.oros
-            "espadas" -> R.drawable.espadas
-            "bastos" -> R.drawable.bastos
-            "copas" -> R.drawable.copas
-            else -> R.drawable.oros // Manejar caso por defecto o desconocido
+            "oros" -> R.mipmap.oros
+            "espadas" -> R.mipmap.espadas
+            "bastos" -> R.mipmap.bastos
+            "copas" -> R.mipmap.copas
+            else -> R.mipmap.oros // Manejar caso por defecto o desconocido
         }
 
         for (i in 0 until 5) {
             for (j in 0 until 5) {
                 if (buttons[i][j].textOff == newText) {
                     // Obtener la imagen actual en drawableEnd
-                    val currentDrawable = buttons[i][j].compoundDrawablesRelative[2]
+                    val currentDrawable = buttons[i][j].compoundDrawablesRelative[3]
 
                     // Comprobar si la imagen actual del botón es igual a la imagen específica
                     if (currentDrawable?.constantState == resources.getDrawable(buttonTextWithSuit)?.constantState) {
@@ -403,22 +374,21 @@ class MainActivity : Activity() {
             }
         }
     }
+    // Función para actualizar el número y el palo de las cartas
     private fun updateButtonText(newText: String,selected: String, button:ToggleButton) {
         val buttonTextWithSuit = when (selected) {
-            "oros" -> R.drawable.oros
-            "espadas" -> R.drawable.espadas
-            "bastos" -> R.drawable.bastos
-            "copas" -> R.drawable.copas
-            else -> R.drawable.oros // Manejar caso por defecto o desconocido
+            "oros" -> R.mipmap.oros
+            "espadas" -> R.mipmap.espadas
+            "bastos" -> R.mipmap.bastos
+            "copas" -> R.mipmap.copas
+            else -> R.mipmap.oros // Manejar caso por defecto o desconocido
         }
         button.textOn = newText
         button.textOff = newText
-        button.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,buttonTextWithSuit,0)
+        button.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0,buttonTextWithSuit)
         // Forzar la actualización del texto
         button.text = newText
         button.isChecked=!button.isChecked
-        // Forzar la actualización de la interfaz de usuario
-        //button.requestLayout()
     }
     private fun checkHorizontalLine(): Boolean {
         for (i in 0 until 5) {
@@ -448,7 +418,6 @@ class MainActivity : Activity() {
         if (vibrator?.hasVibrator() == true) {
             // Patrón de vibración (en milisegundos)
             val pattern = longArrayOf(0, 200, 100, 200)
-            //vibrator.vibrate(pattern, 255)
             vibrator.vibrate(400)
         }
     }
